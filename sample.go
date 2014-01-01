011abcf1b6b9bb83fa632d54cf5ad772e7d51aaa
Zepu Zhang <zepu.zhang@gmail.com>
2013-12-31 23:51:53 -0800

    sample.go: combine Fill and non-Fill functions; remove dependency on package floats

--- sample.go
+++ sample.go
@@ -3,24 +3,19 @@ package stats
 import (
     "math"
     "math/rand"
-    "github.com/gonum/floats"
     "code.google.com/p/probab/dst"
 )
 
 
 
-func Sample(n, k int) []int {
-    idx := make([]int, k)
-    SampleFill(n, k, idx)
-    return idx
-}
-
-
-
-func SampleFill(n, k int, idx []int) {
+func Sample(n, k int, idx []int) []int {
+    if idx == nil {
+        idx = make([]int, k)
+    }
     for i := 0; i < k; i++ {
         idx[i] = rand.Intn(n)
     }
+    return idx[0:k]
 }
 
 
@@ -30,23 +25,16 @@ func SampleFill(n, k int, idx []int) {
 // {0, 1,..., len(w) - 1}, with replacement, according to weights
 // w assigned to the members of the set. The weights in w must sum to 1;
 // this is assumed and not checked.
-func WeightedSample(w []float64, n int) []int {
-    z := make([]int, n)
-    WeightedSampleFill(w, n, z)
-    return z
-}
-
-
-
-
-// WeightedSampleFill returns a random sample in the provide slice idx;
-// otherwise it's identical to WeightedSample.
-func WeightedSampleFill(w []float64, n int, idx []int) {
+func WeightedSample(w []float64, n int, idx []int) []int {
+    if idx == nil {
+        idx = make([]int, n)
+    }
     for i := 0; i < n; i++ {
         idx[i] = int(dst.ChoiceNext(w))
     }
         // FIXME: there might be room for improvement,
         // in a way that generates all n numbers in a batch.
+    return idx[0:n]
 }
 
 
@@ -57,29 +45,17 @@ func WeightedSampleFill(w []float64, n int, idx []int) {
 // logw assigned to the members of the set.
 // The exponentials of the log-weights in logw need not sum to 1,
 // as normalization is performed internally.
-func LogweightedSample(logw []float64, n int) []int {
-    z := make([]int, n)
-    LogweightedSampleFill(logw, n, z)
-    return z
-}
-
-
-
-
-// LogweightedSampleFill returns a random sample in the provided slide idx;
-// otherwise it's identical to LogSample.
-func LogweightedSampleFill(logw []float64, n int, idx []int) {
+func LogweightedSample(logw []float64, n int, idx []int) []int {
     w := make([]float64, len(logw))
-
-    max, _ := floats.Max(logw)
+    max, _ := Max(logw)
     var sum float64
     for i, lw := range logw {
         w[i] = math.Exp(lw - max)
         sum += w[i]
     }
-    floats.Scale(1/sum, w)
+    Scale(w, 1/sum)
 
-    WeightedSampleFill(w, n, idx)
+    return WeightedSample(w, n, idx)
 }
 
 
